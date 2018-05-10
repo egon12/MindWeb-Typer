973f8f6434d867fa57509dcddb6aa0e645042572
egon12 <egon.firman@gmail.com>
2018-05-10 12:48:17 +0700

    Add some error handling and add some test for dag

--- src/dag.js
+++ src/dag.js
@@ -1,67 +1,89 @@
-import SVGPainter from './svg_painter'
-import PositionCalculator from './position_calculator'
+const MAX_LEVEL = 50
 
 export default class DAG {
 
     convertToObj(c) {
+        if (!c) return []
+        
         return c.split("\n").filter(i => i.length > 0).map(r => {
             let newr = r.split(" ")
             return {
                 id: newr.shift(),
-                link: newr
+                link: newr.filter(i => i.length > 0)
             }
         })
     }
 
     getZeroLevel(data) {
-        return  [
-          data.filter(i => i.link.length == 0).map(i => Object.assign({}, i, {y: 0})), 
-          data.filter(i => i.link.length > 0)
-        ]
+        const done = data.filter(i => i.link.length == 0).map(i => Object.assign({}, i, {y: 0}))
+        if (done.length == 0) throw ("There is no independent module")
+        const notyet = data.filter(i => i.link.length > 0)
+        return  [done, notyet]
     }
 
     linkIsIn(done_ids) {
         return obj => {
             if (obj.link.length > done_ids.length) return false
-            for (i in obj.link)
+            for (let i in obj.link)
                 if (!done_ids.includes(obj.link[i])) return false
             return true
         }
     }
 
     getLevel(level, done, notyet) {
-        newdone = notyet
-            .filter(linkIsIn(done.map(i => i.id)))
+        const newdone = notyet
+            .filter(this.linkIsIn(done.map(i => i.id)))
             .map(i => Object.assign({}, i, {y: -1 * level}))
             .concat(done)
         
-        newnotyet = notyet.filter(i => !newdone.map(i => i.id).includes(i.id))
+        const newnotyet = notyet.filter(i => !newdone.map(i => i.id).includes(i.id))
+
+        if (notyet.length == newnotyet.length) throw "Possible cyclic link in " + notyet.map(i=>i.id).join(", ")
 
         return [newdone, newnotyet]
     }
 
+    checkLinkToItSelf(data) {
+        data.forEach(i => {
+            if (i.link.includes(i.id)) {
+                throw i.id + " cannot link to it self"
+            }
+        })
+    }
+
+    checkLinkToNotFound(data) {
+        const all_ids = data.map(i => i.id)
+        data.forEach( d => d.link.forEach( l => {
+            if (!all_ids.includes(l)) {
+                throw "Cannot find link to " + l
+            }
+        }))
+    }
+
+
     setXByLevel(data, maxLevel) {
         for (let y=0; y > -1 * maxLevel; y--) {
-            data.filter(n => n.y == -1 * y).forEach( (d, i) => d.x = i )
+            data.filter(n => n.y == y).forEach( (d, i) => d.x = i )
         }
         return data
     }
 
     process(content) {
-        var done, notyet = this.convertToObj(content);
-        [done, notyet] = this.getZeroLevel(notyet);
-        if (done.length == 0) {
-            console.log("There is no independent module")
-                return []
+        var rawdata = this.convertToObj(content);
+        if (rawdata.length == 0) {
+            return []
         }
+
+        this.checkLinkToItSelf(rawdata)
+
+        this.checkLinkToNotFound(rawdata)
+
+        var [done, notyet] = this.getZeroLevel(rawdata);
+
         let level = 1;
-        let notyetLengthBefore = notyet.length
-        while(notyet.length > 0 ) {
+        while(notyet.length > 0 && level < MAX_LEVEL) {
             [done, notyet] = this.getLevel(level, done, notyet);
             level++;
-            if (notyetLengthBefore == notyet.length) {
-                break; console.log("Cyclic on level " + level);
-            }
         }
 
         return this.setXByLevel(done, level).map(i => Object.assign(i, {color : 'steelblue'}))
---
+++ src/dag.test.js
@@ -0,0 +1,43 @@
+import DAG from './dag'
+
+test("normal operation", () => {
+
+    const content = "Base\nCommon Base\nAccount Common\nMessage Account Common\nCall Account Common"
+
+    const dag = new DAG()
+
+    const r = dag.process(content) 
+    expect(r.find(i => i.id == "Base").y).toBe(0)
+    expect(r.find(i => i.id == "Account").y).toBe(-2)
+    expect(r.find(i => i.id == "Message").x).toBe(0)
+    expect(r.find(i => i.id == "Call").x).toBe(1)
+
+})
+
+test("node link to itself", () => {
+
+    const content = "Base\nCommon Base\nAccount Common Account\n"
+
+    const dag = new DAG()
+
+    expect(() => dag.process(content)).toThrow("Account cannot link to it self")
+})
+
+test("cyclic link ", () => {
+
+    const content = "Base\nCommon Base\nAccount Common Comment\nPost Account\nComment Post\n"
+
+    const dag = new DAG()
+
+    expect(() => dag.process(content)).toThrow("Possible cyclic link in Account, Post, Comment")
+})
+
+
+test("link not available ", () => {
+
+    const content = "Base\nCommon Base\nAccount Common\nPost Account\nComment post\n"
+
+    const dag = new DAG()
+
+    expect(() => dag.process(content)).toThrow("Cannot find link to post")
+})
