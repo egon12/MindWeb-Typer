005953571fc348f5d6a2e792bcbc923f209f2bb8
Egon Firman <egon.firman@gmail.com>
2019-01-05 15:53:33 +0700

    Adding circular calculator

---
+++ src/circular_calculator.js
@@ -0,0 +1,15 @@
+export default class CircularCalculator {
+
+  addRadians(inputTree) {
+
+    
+
+  }
+
+  _addRadians(tree, remainingRadians) {
+
+    tree.children()
+
+  }
+
+}
---
+++ src/circular_calculator.test.js
@@ -0,0 +1,45 @@
+import CircularCalculator from './circular_calculator'
+test('lets try to create something', () => {
+
+  var input = { id: 'Root', children: [
+    { id: 'RootChild1', parentId: 'Root', children: [
+      { id: 'ChildFrom1_1', parentId: 'RootChild1', children: []},
+      { id: 'ChildFrom1_2', parentId: 'RootChild1', children: []},
+      { id: 'ChildFrom1_3', parentId: 'RootChild1', children: []}
+    ] },
+    { id: 'RootChild2', parentId: 'Root', children: [
+      { id: 'ChildFrom2_1', parentId: 'RootChild2', children: []},
+      { id: 'ChildFrom2_2', parentId: 'RootChild2', children: []},
+      { id: 'ChildFrom2_3', parentId: 'RootChild2', children: []}
+    ] }
+  ] }
+
+  var expected = { id: 'Root', children: [
+    { id: 'RootChild1', parentId: 'Root', radians: 0, children: [
+      { id: 'ChildFrom1_1', parentId: 'RootChild1', children: []},
+      { id: 'ChildFrom1_2', parentId: 'RootChild1', children: []},
+      { id: 'ChildFrom1_3', parentId: 'RootChild1', children: []}
+    ] },
+    { id: 'RootChild2', parentId: 'Root', radians: Math.PI, children: [
+      { id: 'ChildFrom2_1', parentId: 'RootChild2', children: []},
+      { id: 'ChildFrom2_2', parentId: 'RootChild2', children: []},
+      { id: 'ChildFrom2_3', parentId: 'RootChild2', children: []}
+    ] }
+  ] }
+
+
+  const cc = new CircularCalculator()
+  const actual = cc.addRadians(input)
+
+
+  expect(actual).toBe(expected)
+
+
+
+
+
+
+
+
+
+})
