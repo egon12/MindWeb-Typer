0b2b16b44f1f8903faa5b2a17b8f6db4b32a34a9
Egon Firman <egon.firman@gmail.com>
2019-01-05 15:52:08 +0700

    Adding tree middleware

--- src/mindmap_processor.js
+++ src/mindmap_processor.js
@@ -1,10 +1,8 @@
-
+import treeMiddleware from './tree_middleware'
 
 export default function mindmapProcessor(inputString) {
 
-  const words = inputString.split('\n').filter(i => i.trim().length > 0)
-
-  const tree = makeTree(words)
+  const tree = treeMiddleware(inputString)
   
   level1PosX = [-10, 10]
   makePosRecursive(tree, 0, tree)
@@ -12,60 +10,21 @@ export default function mindmapProcessor(inputString) {
   return makeTreeToFlat(tree)
 }
 
-function getLevel(word) {
-  return countSpaces(word) / 2
-}
-
-function countSpaces(word) {
-  for (var i=0; i<word.length; i++) {
-    if (word[i] != ' ') {
-      return i;
-    }
-  }
-}
-
 function makeTreeToFlat(tree) {
 
-  const { id, x, y, parentId, childs } = tree
+  const { id, x, y, parentId, children } = tree
 
   const link = parentId ? [parentId] : []
 
   var list = [{ id: id, name: id, x, y, color: 'steelblue', link }]
 
-  for (let i=0; i<childs.length; i++) {
-    list = list.concat(makeTreeToFlat(childs[i]))
+  for (let i=0; i<children.length; i++) {
+    list = list.concat(makeTreeToFlat(children[i]))
   }
 
   return list
 }
 
-
-function makeTree(words) {
-  var level = 0;
-  var root = { id: words[0], childs: [] }
-  var parent = root
-
-  for (let i=1; i<words.length; i++) {
-    const w = words[i].trim()
-    const l = getLevel(words[i])
-
-    const obj = { id: w, childs: [] }
-    
-    if (l > level) {
-      parent = findInTree(root, words[i-1].trim())
-    } else if (l == level) {
-
-    } else if (l < level) {
-      parent = findInTreeLastObjectOnLevel(root, l - 1)
-    }
-
-    obj.parentId = parent.id
-    parent.childs.push(obj)
-    level = l
-  }
-
-  return root
-}
 
 var level1PosX = [-1, 1]
 function makePos(tree, level, root) {
@@ -73,13 +32,13 @@ function makePos(tree, level, root) {
     tree.x = 0;
     tree.y = 0;
 
-    const ang = Math.PI * 2 / tree.childs.length
-    for (let i=0; i<tree.childs.length; i++) {
+    const ang = Math.PI * 2 / tree.children.length
+    for (let i=0; i<tree.children.length; i++) {
       const x = Math.cos(ang * i) * 10
       const y = Math.sin(ang * i) * 10
 
-      tree.childs[i].x = x > 0 ? Math.floor(x) : Math.ceil(x)
-      tree.childs[i].y = y > 0 ? Math.floor(y) : Math.ceil(y)
+      tree.children[i].x = x > 0 ? Math.floor(x) : Math.ceil(x)
+      tree.children[i].y = y > 0 ? Math.floor(y) : Math.ceil(y)
     }
 
     return
@@ -94,28 +53,27 @@ function makePos(tree, level, root) {
 
   if (parent.x > 0) {
     tree.x = parent.x + 10
-    tree.y = parent.y + parent.childs.indexOf(tree) - Math.floor(parent.childs.length / 2)
+    tree.y = parent.y + parent.children.indexOf(tree) - Math.floor(parent.children.length / 2)
   } else {
     tree.x = parent.x - 10
-    tree.y = parent.y + parent.childs.indexOf(tree) - Math.floor(parent.childs.length / 2)
+    tree.y = parent.y + parent.children.indexOf(tree) - Math.floor(parent.children.length / 2)
   }
 }
 
 function makePosRecursive(tree, level, root) {
   makePos(tree, level, root)
-  for (let i=0; i<tree.childs.length; i++) {
-    makePosRecursive(tree.childs[i], level+1, tree)
+  for (let i=0; i<tree.children.length; i++) {
+    makePosRecursive(tree.children[i], level+1, tree)
   }
 }
 
 function findInTree(tree, id) {
-
   if (tree.id == id) {
     return tree
   }
 
-  for (let i=0; i<tree.childs.length; i++) {
-    const result = findInTree(tree.childs[i], id)
+  for (let i=0; i<tree.children.length; i++) {
+    const result = findInTree(tree.children[i], id)
     if (result != null) {
       return result
     }
@@ -124,16 +82,3 @@ function findInTree(tree, id) {
   return null
 }
 
-function findInTreeLastObjectOnLevel(tree, level) {
-  if (level == 0) {
-    return tree
-  }
-
-  const lastIndex = tree.childs.length - 1
-
-  if (level == 1) {
-    return tree.childs[lastIndex]
-  }
-
-  return findInTreeLastObjectOnLevel(tree.childs[lastIndex], level - 1)
-}
---
+++ src/tree_middleware.test.js
@@ -0,0 +1,30 @@
+import treeMiddleware from './tree_middleware'
+
+test('Should create tree from indented string', () => {
+
+  var expected = { id: 'Root', children: [
+    { id: 'RootChild1', parentId: 'Root', children: [
+      { id: 'ChildFrom1_1', parentId: 'RootChild1', children: []},
+      { id: 'ChildFrom1_2', parentId: 'RootChild1', children: []},
+      { id: 'ChildFrom1_3', parentId: 'RootChild1', children: []}
+    ] },
+    { id: 'RootChild2', parentId: 'Root', children: [
+      { id: 'ChildFrom2_1', parentId: 'RootChild2', children: []},
+      { id: 'ChildFrom2_2', parentId: 'RootChild2', children: []},
+      { id: 'ChildFrom2_3', parentId: 'RootChild2', children: []}
+    ] }
+  ] }
+
+  const input = `
+Root
+  RootChild1
+    ChildFrom1_1
+    ChildFrom1_2
+    ChildFrom1_3
+  RootChild2
+    ChildFrom2_1
+    ChildFrom2_2
+    ChildFrom2_3
+`;
+  expect(treeMiddleware(input)).toEqual(expected);
+});
