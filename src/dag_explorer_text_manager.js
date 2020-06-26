c82cc5e8e53da087023ad4a115b9e04e0c22f0db
Egon Firman <egon.firman@tokopedia.com>
2020-06-26 20:11:07 +0700

    using real object in dag_text_manager

--- src/dag_explorer_text_manager.js
+++ src/dag_explorer_text_manager.js
@@ -1,6 +1,7 @@
 export default class DagExplorerTextManager {
 	constructor(content) {
 		this.content = content;
+		this.allNodes = constructObject(this.content)
 	}
 
 	getRoot() {
@@ -8,17 +9,16 @@ export default class DagExplorerTextManager {
 	}
 
 	getID(idStr) {
-		const arrOfArr = this.content
-			.split("\n")
-			.map(i => i.split(" ").filter(i => i.length > 0));
+		const node = this.allNodes[idStr]
 
-		const selectedNode = arrOfArr.find(i => i[0] == idStr);
+		const name = node.name
 
-		const dependToNode = arrOfArr
-			.filter(i => i.indexOf(idStr) > -1 && i[0] != idStr)
-			.map(i => [i[0], idStr]);
+		const selectedNode = node.dependencies.map(i => i.name)
+		selectedNode.unshift(name)
 
-		const needByNode = selectedNode.filter(i => i != idStr).map(i => [i]);
+		const dependToNode = node.importBy.map(i => [i.name, name])
+
+		const needByNode = node.dependencies.map(i => [i.name])
 
 		return [selectedNode]
 			.concat(dependToNode)
@@ -26,4 +26,113 @@ export default class DagExplorerTextManager {
 			.map(i => i.join(" "))
 			.join("\n");
 	}
+
+	shouldBeGroup(input1, input2) {}
+
+	uniqueTree(exclude) {
+		const mostBound = this.getNodeNotImported()
+
+		isBounded(mostBound[0], mostBound[1], exclude || [])
+	}
+
+	getAllDependencies(idStr) {
+		const node = this.allNodes[idStr]
+	}
+
+	getFreeDependencies() {
+		let result = []
+		for (let key in this.allNodes) {
+			const node = this.allNodes[key]
+			if (node.dependencies.length == 0) {
+				result.push(node)
+			}
+		}
+		return result
+	}
+
+	getNodeNotImported() {
+		let result = []
+		for (let key in this.allNodes) {
+			const node = this.allNodes[key]
+			if (node.importBy.length == 0) {
+				result.push(node)
+			}
+		}
+		return result
+	}
+
+	isBounded(name1, name2, exclude) {
+		const node1 = this.allNodes[name1]
+		const node2 = this.allNodes[name2]
+		const excludeNodes = exclude ? exclude.map(i => this.allNodes[i]): []
+		
+		return isBounded(node1, node2, excludeNodes)
+	}
+}
+
+function constructObject(content) {
+	const arrOfArr = content
+		.split("\n")
+		.map(i => i.split(" ").filter(i => i.length > 0))
+		.filter(i => i.length > 0);
+
+	const allNodes = {}
+
+	arrOfArr
+		.map(i => i[0])
+		.forEach(i => allNodes[i] = {name: i, dependencies: [], importBy: []})
+
+	arrOfArr.filter(i => i.length > 1).forEach(i => {
+		const node = allNodes[i[0]]
+
+		for (let j=1; j<i.length; j++) {
+			const name = i[j]
+
+			if (node.name == name) {
+				throw new Error("Cannot import it self")
+			}
+
+			const dependencyNode = allNodes[name]
+			if (!dependencyNode) {
+				throw new Error("Cannot find " + name)
+			}
+
+			dependencyNode.importBy.push(node)
+			node.dependencies.push(dependencyNode)
+		}
+	})
+
+	return allNodes
+}
+
+
+function isBounded(node1, node2, excluded) {
+
+	let found = node1.importBy.find(i => i == node2)
+	if (found) return true
+
+	found = node1.dependencies.find(i => i == node2)
+	if (found) return true
+
+	excluded.push(node1)
+
+	for (let i=0; i<node1.importBy.length; i++) {
+		const node1link = node1.importBy[i]
+		if (excluded.find(i => i == node1link)) {
+			continue
+		}
+		found = isBounded(node1link, node2, excluded)
+		if (found) return true
+	}
+
+	for (let i=0; i<node1.dependencies.length; i++) {
+		const node1link = node1.dependencies[i]
+		if (excluded.find(i => i == node1link)) {
+			continue
+		}
+		found = isBounded(node1link, node2, excluded)
+		if (found) return true
+	}
+
+	return false
 }
--- src/dag_explorer_text_manager.test.js
+++ src/dag_explorer_text_manager.test.js
@@ -1,7 +1,7 @@
 import DagExplorerTextManager from "./dag_explorer_text_manager.js";
 
 test("getRoot should get all content", () => {
-	const content = "A B C\nB C";
+	const content = "A B C\nB C\nC";
 
 	const tm = new DagExplorerTextManager(content);
 
@@ -9,10 +9,90 @@ test("getRoot should get all content", () => {
 });
 
 test("getID should filter that have that id in dependencies", () => {
-	const content = "A B C\nB C\nD C\nE F\nF B Z";
+	const content = "A B C\nB C\nD C\nE F\nF B Z\nC\nZ";
 	const expected = "B C\nA B\nF B\nC";
 
 	const tm = new DagExplorerTextManager(content);
 
 	expect(tm.getID("B")).toEqual(expected);
 });
+
+test.skip('can be group', () => {
+	const input1 = 'a/b/c/d'
+	const input2 = 'a/b/c/e'
+	const input3 = 'a/b/c/f/g/h'
+	const input4 = 'x/y/z'
+	const input5 = 'x/y/y'
+	const input6 = 'a/b/c/g/h'
+
+	const tm = new DagExplorerTextManager('');
+
+	expect(tm.canBeGroup(input1, input2)).toBe(true);
+	expect(tm.canBeGroup(input1, input3)).toBe(true);
+	expect(tm.canBeGroup(input2, input3)).toBe(true);
+	expect(tm.canBeGroup(input6, input3)).toBe(true);
+	
+	expect(tm.canBeGroup(input1, input4)).toBe(false);
+	expect(tm.canBeGroup(input1, input5)).toBe(false);
+
+
+});
+
+test('check is bounded', () => {
+	const content = `
+B
+A B C D
+D E F
+G H I
+J B
+C
+H
+I
+E
+F
+`
+
+	const tm = new DagExplorerTextManager(content);
+	expect(tm.isBounded('A', 'B')).toBe(true)
+	expect(tm.isBounded('A', 'J')).toBe(true)
+	expect(tm.isBounded('A', 'J', ['B'])).toBe(false)
+	expect(tm.isBounded('A', 'G')).toBe(false)
+	expect(tm.isBounded('A', 'I')).toBe(false)
+})
+
+
+test.skip('uniqe tree', () => {
+	const content = `
+B
+A B C D
+D E F
+G H I
+J B
+C
+H
+I
+E
+F
+`
+
+	const tm = new DagExplorerTextManager(content);
+	expect(tm.uniqueTree()).toEqual(['A|J', 'G'])
+})
+
+
+test('get edge of node', () => {
+	const content = `
+A B C D
+B
+C
+D E F
+E
+F G
+G
+`
+
+	const tm = new DagExplorerTextManager(content);
+
+	expect(tm.getNodeNotImported().map(i => i.name)).toEqual(['A'])
+	expect(tm.getFreeDependencies().map(i => i.name)).toEqual(['B', 'C', 'E', 'G'])
+})
