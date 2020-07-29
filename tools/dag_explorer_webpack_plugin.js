8aa17196417f39f8e7bef47ddfac1810cb9fee86
Egon Firman <egon.firman@tokopedia.com>
2020-07-30 00:31:07 +0700

    using ejs for template

--- tools/dag_explorer_webpack_plugin.js
+++ tools/dag_explorer_webpack_plugin.js
@@ -1,33 +1,17 @@
 const fs = require("fs");
 
-const html = `<!doctype html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<style> html, body { height: "100%"; margin: 0; padding: 0; } </style>
-</head>
-<body>
-<div id="container" style="margin:'5px';"></div>
-<button id="reset" type="button" style="position:fixed;top:0;">Reset</button>
-<script>
-window.dag_content = "{{.DAG}}"
-</script>
-{{.script}}
-</body>
-</html>
-`;
+const html = fs.readFileSync(__dirname + '/../src/dag_explorer.ejs').toString()
 
 class DagExplorerWebpackPlugin {
 	apply(compiler) {
 		const hooks = compiler.hooks;
 		hooks.afterEmit.tap("create-dag-explorer-html", data => {
-			const script =
-				'<script type="text/javascript">' +
+			const script = '<script>window.dag_content = "{{.DAG}}"</script><script type="text/javascript">' +
 				data.assets["dag_explorer.js"]._value +
 				"</script>";
 			fs.writeFileSync(
 				__dirname + "/../html/dag_explorer_empty.html",
-				html.replace("{{.script}}", script)
+				html.replace(/<script>.*<\/script>/, script)
 			);
 		});
 	}
